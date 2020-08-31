5cff82c274de196bdd970e5dd7ab65e37d81d43a
pradeep reddy <pradeepreddyyendrapalligmail.com>
2020-08-31 02:58:37 -0700

    python program

--- file4.py
+++ file4.py
@@ -1,9 +1,10 @@
+import os
 # this is a sample test file1
 ax = str(10)
 print(" value of x :" + ax)
 
-x = int(input("x value: "))
-y = int(input("y value: "))
+# x = int(input("x value: "))
+# y = int(input("y value: "))
 
 
 def function1(x,y):
@@ -17,3 +18,10 @@ def function1(x,y):
 function1(1,0)
 
 print("completed the if loop")
+
+f = open("file4.txt", "r")
+print(f.readline())
+print(f.readline())
+# f.write("\n line 7")
+f.close()
+os.remove("file4.txt")
