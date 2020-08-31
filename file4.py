b064a2da433eca2a11ade225e469bbc799c8b5cd
pradeep reddy <pradeepreddyyendrapalligmail.com>
2020-08-31 02:37:29 -0700

    test change

--- file4.py
+++ file4.py
@@ -1,3 +1,19 @@
 # this is a sample est fle s
-x = str(10)
-print(" value of x :" + x)
+ax = str(10)
+print(" value of x :" + ax)
+
+x = int(input("x value: "))
+y = int(input("y value: "))
+
+
+def function1(x,y):
+    if x == 10:
+        print("x is 10")
+    elif y == 20:
+        print("y is 20")
+    else:
+        print("x is nt 10")
+    return x
+function1(1,0)
+
+print("completed the lop")
